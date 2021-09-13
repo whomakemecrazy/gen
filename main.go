5398ef64a2ed7d39d57312a39b40c1d61391560c
ray <ray@sentbe.com>
2021-09-13 22:50:43 +0900

    update

--- main.go
+++ main.go
@@ -1,33 +1,34 @@
 package main
 
 import (
-  "flag"
-  "os"
-  "text/template"
+	"flag"
+	"os"
+	"text/template"
 )
 
 func main() {
-  var pkg, srv, entity string
-  flag.StringVar(&pkg, "p", "pkg" , "-p pkgName")
-  flag.StringVar(&srv, "s", "srv", "-s serviceName")
-  flag.StringVar(&entity, "e", "entity", "-s entityName")
-  
-  values := New(pkg, srv, entity)
-  
-  tpl, err := template.New("meta").Parse(code)
-  if err != nil {
-    panic(err)
-  }
-  
-  var f *os.File
-  
-  f, err = os.Create("fp.go")
-  if err != nil {
-    panic(err)
-  }
-  err = tpl.Execute(f, values)
-  if err != nil {
-    panic(err)
-  }
-  f.Close()
+	var pkg, srv, entity string
+	flag.StringVar(&pkg, "p", "pkg", "pkgName")
+	flag.StringVar(&srv, "s", "srv", "serviceName")
+	flag.StringVar(&entity, "e", "entity", "entityName")
+	flag.Parse()
+
+	values := New(pkg, srv, entity)
+
+	tpl, err := template.New("meta").Parse(code)
+	if err != nil {
+		panic(err)
+	}
+
+	var f *os.File
+
+	f, err = os.Create("fp.go")
+	if err != nil {
+		panic(err)
+	}
+	err = tpl.Execute(f, values)
+	if err != nil {
+		panic(err)
+	}
+	f.Close()
 }
